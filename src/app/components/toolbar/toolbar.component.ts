fc298c802166a1fed1313caacd26baa9c98233ee
Matias Estrada <matiasunju1@gmail.com>
2025-06-13 17:46:23 -0300

    toolbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+const MATERIAL_MODULE = [MatToolbarModule, MatIconModule, MatButtonModule];
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [MATERIAL_MODULE],
   template: `
-    <p>
-      toolbar works!
-    </p>
+    <mat-toolbar color="primary">
+      <a mat-button routerLink="/">
+        <mat-icon>home</mat-icon>
+        <span>Home</span></a>
+
+        <a mat-button routerLink="/contact">
+        <mat-icon>list_alt</mat-icon>
+        <span>Contacts</span></a>
+
+        <a mat-button>
+        <mat-icon>add_box</mat-icon>
+        <span>New</span></a>
+    </mat-toolbar>
   `,
   styles: ``
 })
