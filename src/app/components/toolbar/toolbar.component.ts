81d7837c7c872c6100f7e9cbe70ebf839038042d
Matias Estrada <matiasunju1@gmail.com>
2025-06-15 19:24:04 -0300

    output

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -14,17 +14,25 @@ const MATERIAL_MODULE = [MatToolbarModule, MatIconModule, MatButtonModule];
         <mat-icon>home</mat-icon>
         <span>Home</span></a>
 
-        <a mat-button routerLink="/contact">
+      <a mat-button routerLink="/contact">
         <mat-icon>list_alt</mat-icon>
         <span>Contacts</span></a>
+      <span class="spacer"></span>
 
-        <a mat-button>
+      <a mat-button (click)="emitClick()")>
         <mat-icon>add_box</mat-icon>
         <span>New</span></a>
     </mat-toolbar>
   `,
-  styles: ``
+
 })
 export class ToolbarComponent {
+  onNewContactEvent = output<void>();
+  emitClick(): void{
+    this.onNewContactEvent.emit();
+
+  }
+  
+  
 
 }
