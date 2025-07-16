169b15ac24c421cbb647446d3efb8e70411012ef
Matias Estrada <matiasunju1@gmail.com>
2025-07-15 21:38:21 -0300

    firebase config

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,11 @@
 export const environment = {
-    firebase:{}
+    firebase:{
+         apiKey: "AIzaSyC9LXaQVEE3yL2MEcGNPGZGysknMUKvmuM",
+        authDomain: "angular--18--crud.firebaseapp.com",
+        projectId: "angular--18--crud",
+        storageBucket: "angular--18--crud.firebasestorage.app",
+        messagingSenderId: "210689048015",
+        appId: "1:210689048015:web:bb10651c4f8e78b11666ba",
+        measurementId: "G-1SE6TBG55Q"
+    }
 };
