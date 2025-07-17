4dd581140e6e63ea522ad1d89e4841788ce15ae9
Matias Estrada <matiasunju1@gmail.com>
2025-07-17 17:01:45 -0300

    added

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -3,13 +3,17 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { MAT_DIALOG_DATA, MatDialogContent, MatDialogModule } from '@angular/material/dialog';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
+import { ContactService } from '@features/contact/contact.service';
+import { ModalService } from './modal.service';
+import { APP_CONSTANTS } from '@shared/constants';
+import { MatButtonModule } from '@angular/material/button';
 
-const MATERIAL_MODULES = [MatLabel, MatFormField, MatInput, MatDialogModule]
+const MATERIAL_MODULES = [MatLabel, MatFormField, MatInput, MatDialogModule,MatButtonModule]
 @Component({
   selector: 'app-modal',
   imports: [ReactiveFormsModule, MATERIAL_MODULES],
   templateUrl: './modal.component.html',
-  styles: ``
+  styleUrl: './modal.component.scss'
 })
 export class ModalComponent implements OnInit{
   
@@ -17,13 +21,39 @@ export class ModalComponent implements OnInit{
 
   private readonly _fb = inject(FormBuilder);
   private readonly _matDialog = inject(MAT_DIALOG_DATA);
+  private readonly _contactSvc = inject(ContactService);
+  private readonly _modalSvc = inject(ModalService);
 
   ngOnInit(): void {
     this._builForm();
+
+    if (this._matDialog.isEditing){
+      this.contactForm.patchValue(this._matDialog.data);
+      this._disabledForm();
+    }
+  }
+
+  async onSubmit(){
+    let message = APP_CONSTANTS.MESSAGES.CONTACT_UPDATED;
+    const contact = this.contactForm.value;
+    if (this._matDialog.data){
+      this._contactSvc.updateContact(this._matDialog.data.id, contact);
+    }else{
+     await this._contactSvc.newContact(contact);
+      message = APP_CONSTANTS.MESSAGES.CONTACT_ADDED;
+    }
+
+    // show snackBar
+    console.log(message);
+    this._contactSvc.newContact(contact);
   }
 
   getTitle(): string {
     return this._matDialog.data ? 'Edit Contact' : 'Add Contact';
+  }
+
+  private _disabledForm(): void {
+    this.contactForm.disable();
   }
 
   private _builForm(): void{
